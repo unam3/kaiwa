f452134e7140d9b865c4448660250cd957cdd1c7
Friedrich von Never <friedrich@fornever.me>
2015-09-05 12:05:05 +0600

    Update push notification API.

--- clientapp/helpers/pushNotifications.js
+++ clientapp/helpers/pushNotifications.js
@@ -1,48 +1,54 @@
 "use strict";
 
-var jxt = require('jxt');
-var stanzaio = require('stanza.io');
+module.exports = function (client, stanzas) {
+    var types = stanzas.utils;
 
+    var PushNotification = stanzas.define({
+        name: 'pushNotification',
+        namespace: 'urn:xmpp:push:0',
+        element: 'push',
+        fields: {
+            body: types.subText('urn:xmpp:push:0', 'body')
+        }
+    });
 
-jxt.extend(stanzaio.Message, jxt.define({
-    name: 'pushNotification',
-    namespace: 'urn:xmpp:push:0',
-    element: 'push',
-    fields: {
-        body: jxt.subText('urn:xmpp:push:0', 'body')
-    }
-}));
+    stanzas.withMessage(function (Message) {
+        stanzas.extend(Message, PushNotification);
+    });
 
-jxt.extend(stanzaio.Iq, jxt.define({
-    name: 'registerPush',
-    namespace: 'urn:xmpp:push:0',
-    element: 'register',
-    fields: {
-        service: jxt.text()
-    }
-}));
+    var RegisterPush = stanzas.define({
+        name: 'registerPush',
+        namespace: 'urn:xmpp:push:0',
+        element: 'register',
+        fields: {
+            service: types.text()
+        }
+    });
 
-jxt.extend(stanzaio.Iq, jxt.define({
-    name: 'unregisterPush',
-    namespace: 'urn:xmpp:push:0',
-    element: 'unregister',
-    fields: {
-        service: jxt.text()
-    }
-}));
+    var UnregisterPush = stanzas.define({
+        name: 'unregisterPush',
+        namespace: 'urn:xmpp:push:0',
+        element: 'unregister',
+        fields: {
+            service: types.text()
+        }
+    });
 
+    var OtalkRegister = stanzas.define({
+        name: 'otalkRegister',
+        namespace: 'http://otalk.im/protocol/push',
+        element: 'register',
+        fields: {
+            deviceID: types.text()
+        }
+    });
 
-jxt.extend(stanzaio.Iq, jxt.define({
-    name: 'otalkRegister',
-    namespace: 'http://otalk.im/protocol/push',
-    element: 'register',
-    fields: {
-        deviceID: jxt.text()
-    }
-}));
+    stanzas.withIq(function (Iq) {
+        stanzas.extend(Iq, RegisterPush);
+        stanzas.extend(Iq, UnregisterPush);
+        stanzas.extend(Iq, OtalkRegister);
+    });
 
-
-module.exports = function (client) {
     client.registerPushService = function (jid, cb) {
         return client.sendIq({
             type: 'set',
