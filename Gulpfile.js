58002f87a0ec367253cfcad152840442789f2132
Friedrich von Never <friedrich@fornever.me>
2015-09-08 23:08:50 +0600

    Compile most of the application statically.

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,5 +1,8 @@
 var browserify = require('browserify');
 var fs = require('fs');
+var concatCss = require('gulp-concat-css');
+var config = require('./dev_config');
+var jade = require('gulp-jade');
 var gulp = require('gulp');
 var templatizer = require('templatizer');
 
@@ -8,11 +11,33 @@ gulp.task('resources', function () {
     return gulp.src('./src/resources/**')
         .pipe(gulp.dest('./public'));
 });
-gulp.task('client', ['jade'], function (cb) {
+gulp.task('client', ['jade-templates', 'jade-views'], function (cb) {
     return browserify({
         entries: [ './src/js/app.js' ]
     }).bundle().pipe(fs.createWriteStream('./public/js/app.js'));
 });
-gulp.task('jade', function (cb) {
+gulp.task('jade-templates', function (cb) {
     templatizer('./src/jade/templates', './src/js/templates.js', cb);
+});
+gulp.task('jade-views', ['jade-views-login', 'css'], function () {
+    return gulp.src([
+        './src/jade/views/error.jade',
+        './src/jade/views/index.jade',
+        './src/jade/views/logout.jade',
+        './src/jade/views/oauthLogin.jade'
+    ]).pipe(jade()).pipe(gulp.dest('./public/'));
+});
+gulp.task('jade-views-login', ['css'], function () {
+    return gulp.src('./src/jade/views/login.jade')
+        .pipe(jade({
+            locals: {
+                config: config.server
+            }
+        }))
+        .pipe(gulp.dest('./public/'));
+});
+gulp.task('css', function () {
+    return gulp.src('./src/css/*.css')
+        .pipe(concatCss('app.css'))
+        .pipe(gulp.dest('./public/css/'));
 });
--- src/server.js
+++ src/server.js
@@ -3,13 +3,10 @@ var https = require('https');
 var express = require('express');
 var helmet = require('helmet');
 var Moonboots = require('moonboots-express');
-var config = require('getconfig');
 var templatizer = require('templatizer');
 var async = require('async');
 
-String.prototype.capitalize = function() {
-    return this.charAt(0).toUpperCase() + this.slice(1);
-}
+var config = JSON.parse(fs.readFileSync('./dev_config.json'));
 
 var app = express();
 var bodyParser = require('body-parser')
@@ -19,7 +16,7 @@ var serveStatic = require('serve-static');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(compression());
-app.use(serveStatic(__dirname + '/public'));
+app.use(serveStatic('./public'));
 if (!config.isDev) {
     app.use(helmet.xframe());
 }
@@ -28,26 +25,9 @@ app.use(helmet.contentTypeOptions());
 
 var webappManifest = fs.readFileSync('./public/x-manifest.webapp');
 
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-
-app.get('/login', function (req, res) {
-    res.render("login", {"config": config.server});
-});
-
-app.get('/logout', function (req, res) {
-    res.render('logout');
-});
-
 app.get('/config.js', function (req, res) {
     res.type('application/javascript');
     res.send("var SERVER_CONFIG = " + JSON.stringify(config.server) + ";");
-});
-
-app.get('/sounds/*', function (req, res) {
-    console.log(req.baseUrl);
-    res.type('audio/wav');
-    res.redirect("./public" + req.baseUrl);
 });
 
 app.get('/manifest.webapp', function (req, res, next) {
@@ -74,64 +54,59 @@ app.use(function handleError(err, req, res, next) {
     res.render('error', errorResult);
 });
 
-var clientApp = new Moonboots({
-    moonboots: {
-        main: __dirname + '/clientapp/app.js',
-        developmentMode: config.isDev,
-        libraries: [
-            __dirname + '/clientapp/libraries/jquery.js',
-            __dirname + '/clientapp/libraries/ui.js',
-            __dirname + '/clientapp/libraries/resampler.js',
-            __dirname + '/clientapp/libraries/IndexedDBShim.min.js',
-            __dirname + '/clientapp/libraries/sugar-1.2.1-dates.js',
-            __dirname + '/clientapp/libraries/jquery.oembed.js',
-            __dirname + '/clientapp/libraries/jquery-impromptu.js'
-        ],
-        browserify: {
-            debug: false
-        },
-        stylesheets: [
-            __dirname + '/public/css/client.css',
-            __dirname + '/public/css/jquery.oembed.css',
-            __dirname + '/public/css/jquery-impromptu.css'
-        ],
-        beforeBuildJS: function () {
-            if (config.isDev) {
-                var clientFolder = __dirname + '/clientapp';
-                templatizer(clientFolder + '/templates', clientFolder + '/templates.js');
-            }
-        }
-    },
-    server: app,
-    cachePeriod: 0,
-    render: function (req, res) {
-        res.render('index');
-    }
-});
-
-clientApp.on('ready', function () {
-    console.log('Client app ready');
-    var pkginfo = JSON.parse(fs.readFileSync(__dirname + '/package.json'));
-
-    var manifestTemplate = fs.readFileSync(__dirname + '/clientapp/templates/misc/manifest.cache', 'utf-8');
-    var cacheManifest = manifestTemplate
-          .replace('#{version}', pkginfo.version + config.isDev ? ' ' + Date.now() : '')
-          .replace('#{jsFileName}', clientApp.moonboots.jsFileName())
-          .replace('#{cssFileName}', clientApp.moonboots.cssFileName());
-    console.log('Cache manifest generated');
-
-
-    app.get('/manifest.cache', function (req, res, next) {
-        res.set('Content-Type', 'text/cache-manifest');
-        res.set('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
-        res.send(cacheManifest);
-    });
-
-    // serves app on every other url
-    app.get('*', function (req, res) {
-        res.render(clientApp.moonboots.htmlSource());
-    });
-});
+// TODO: Setup config and manifest building.
+// var clientApp = new Moonboots({
+//     moonboots: {
+//         main: __dirname + '/clientapp/app.js',
+//         developmentMode: config.isDev,
+//         libraries: [
+//             __dirname + '/clientapp/libraries/jquery.js',
+//             __dirname + '/clientapp/libraries/ui.js',
+//             __dirname + '/clientapp/libraries/resampler.js',
+//             __dirname + '/clientapp/libraries/IndexedDBShim.min.js',
+//             __dirname + '/clientapp/libraries/sugar-1.2.1-dates.js',
+//             __dirname + '/clientapp/libraries/jquery.oembed.js',
+//             __dirname + '/clientapp/libraries/jquery-impromptu.js'
+//         ],
+//         browserify: {
+//             debug: false
+//         },
+//         stylesheets: [
+//             __dirname + '/public/css/client.css',
+//             __dirname + '/public/css/jquery.oembed.css',
+//             __dirname + '/public/css/jquery-impromptu.css'
+//         ]
+//     },
+//     server: app,
+//     cachePeriod: 0,
+//     render: function (req, res) {
+//         res.render('index');
+//     }
+// });
+//
+// clientApp.on('ready', function () {
+//     console.log('Client app ready');
+//     var pkginfo = JSON.parse(fs.readFileSync(__dirname + '/package.json'));
+//
+//     var manifestTemplate = fs.readFileSync(__dirname + '/clientapp/templates/misc/manifest.cache', 'utf-8');
+//     var cacheManifest = manifestTemplate
+//           .replace('#{version}', pkginfo.version + config.isDev ? ' ' + Date.now() : '')
+//           .replace('#{jsFileName}', clientApp.moonboots.jsFileName())
+//           .replace('#{cssFileName}', clientApp.moonboots.cssFileName());
+//     console.log('Cache manifest generated');
+//
+//
+//     app.get('/manifest.cache', function (req, res, next) {
+//         res.set('Content-Type', 'text/cache-manifest');
+//         res.set('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
+//         res.send(cacheManifest);
+//     });
+//
+//     // serves app on every other url
+//     app.get('*', function (req, res) {
+//         res.render(clientApp.moonboots.htmlSource());
+//     });
+// });
 
 //https.createServer({
 //    key: fs.readFileSync(config.http.key),
