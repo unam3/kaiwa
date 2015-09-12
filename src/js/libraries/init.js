cf3c12ba5ffcc836e1b60baefcf39f70a7593f0f
Friedrich von Never <friedrich@fornever.me>
2015-09-12 10:54:26 +0600

    Drop unused libraries. Close #33.

--- src/js/libraries/init.js
+++
@@ -1,9 +0,0 @@
-$('head').prepend([
-    '<link rel="stylesheet" href="/style.css">',
-//    '<link rel="stylesheet" href="/fonts.css">',
-//    '<link rel="stylesheet" href="/ui.css">',
-].join(''))
-
-$(function () {
-    require('app').launch();
-});
